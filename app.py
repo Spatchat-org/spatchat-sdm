4fe633da52fdaa50b231029465510b054203bb1f
Ho Yi Wan <hoyiwan@users.noreply.huggingface.co>
2025-05-05 01:39:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -470,7 +470,7 @@ with gr.Blocks() as demo:
     gr.Markdown("## 🗺️ SpatChat: Species Distribution Model {sdm}  🐢🐍🦅🦋🦉🦊🐞 ")
     gr.HTML("""
     <div style="margin-top: -10px; margin-bottom: 15px;">
-      <input type="text" value="hhttps://spatchat.org/browse/?room=sdm" id="shareLink" readonly style="width: 50%; padding: 5px; background-color: #f8f8f8; color: #222; font-weight: 500; border: 1px solid #ccc; border-radius: 4px;">
+      <input type="text" value="https://spatchat.org/browse/?room=sdm" id="shareLink" readonly style="width: 50%; padding: 5px; background-color: #f8f8f8; color: #222; font-weight: 500; border: 1px solid #ccc; border-radius: 4px;">
       <button onclick="navigator.clipboard.writeText(document.getElementById('shareLink').value)" style="padding: 5px 10px; background-color: #007BFF; color: white; border: none; border-radius: 4px; cursor: pointer;">
         📋 Copy Share Link
       </button>
